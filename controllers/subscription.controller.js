287e0e51a1853ec752a6a5a43e844db987a0c9c1
Ajiboye <ajiboyemuyideen7@gmail.com>
2026-03-17 19:52:28 +0100

    rebase

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -29,7 +29,7 @@ export const createSubscription = async (req, res, next) => {
 export const getUserSubscriptions = async (req, res, next) => {
   try {
     if(req.user.id !== req.params.id) {
-      const error = new Error('You are not the owner of this account, please use your account');
+      const error = new Error('You are not the owner of this account');
       error.status = 401;
       throw error;
     }
