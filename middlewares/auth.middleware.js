ab13f0b586433c56ee229c9ebe15cec2db9a3b5f
Ajiboye <ajiboyemuyideen7@gmail.com>
2026-03-24 13:17:44 +0100

    clean up

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -11,7 +11,7 @@ const authorize = async (req, res, next) => {
       token = req.headers.authorization.split(' ')[1];
     }
 
-    if(!token) return res.status(401).json({ message: 'Unauthorized! ' });
+    if(!token) return res.status(401).json({ message: 'Unauthorized!' });
 
     const decoded = jwt.verify(token, JWT_SECRET);
 
