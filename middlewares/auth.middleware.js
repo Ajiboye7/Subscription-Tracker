4e2dbf9e2cf1965c9beb0075b007a75d0de2da45
Ajiboye <ajiboyemuyideen7@gmail.com>
2026-04-22 19:31:48 +0100

    fix auth issue

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -11,13 +11,13 @@ const authorize = async (req, res, next) => {
       token = req.headers.authorization.split(' ')[1];
     }
 
-    if(!token) return res.status(401).json({ message: 'Unauthorized!' });
+    if(!token) return res.status(401).json({ message: 'Unauthorized' });
 
     const decoded = jwt.verify(token, JWT_SECRET);
 
     const user = await User.findById(decoded.userId);
 
-    if(!user) return res.status(401).json({ message: 'Unauthorized!' });
+    if(!user) return res.status(401).json({ message: 'Unauthorized' });
 
     req.user = user;
 
