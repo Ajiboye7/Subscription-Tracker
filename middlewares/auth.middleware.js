3ca2fbf1830d41804ed9fd6b45e9f81cc6356a11
Ajiboye <ajiboyemuyideen7@gmail.com>
2026-05-19 19:10:26 +0100

    authorization

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -23,7 +23,7 @@ const authorize = async (req, res, next) => {
 
     next();
   } catch (error) {
-    res.status(401).json({ message: 'Unauthorized!', error: error.message });
+    res.status(401).json({ message: 'Unauthorized', error: error.message });
   }
 }
 
